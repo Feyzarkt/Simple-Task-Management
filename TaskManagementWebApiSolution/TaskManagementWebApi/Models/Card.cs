dbaee58d6a2bade2424ef2f8c2d17c03a98611a9
Feyza Ürkut <60755254+Feyzarkt@users.noreply.github.com>
2022-05-30 21:59:09 +0300

    add project files

---
+++ TaskManagementWebApiSolution/TaskManagementWebApi/Models/Card.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManagementWebApi.Models
+{
+    public class Card
+    {
+        public Guid CardId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Guid BoardId { get; set; }
+        public DateTime Deadline { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
