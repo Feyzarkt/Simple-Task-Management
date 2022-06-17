c407c5febd656dc82aa73e2c4207405bf073ccff
Feyzarkt <60755254+Feyzarkt@users.noreply.github.com>
2022-06-17 15:33:28 +0300

    add nullables

--- TaskManagementWebApiSolution/TaskManagementWebApi/Models/Card.cs
+++ TaskManagementWebApiSolution/TaskManagementWebApi/Models/Card.cs
@@ -2,12 +2,12 @@
 {
     public class Card
     {
-        public Guid CardId { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public Guid? CardId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
         public Guid BoardId { get; set; }
         public DateTime Deadline { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
 
     }
 }
--- TaskManagementWebApiSolution/TaskManagementWebApi/Models/User.cs
+++ TaskManagementWebApiSolution/TaskManagementWebApi/Models/User.cs
@@ -3,9 +3,9 @@
     public class User
     {
         public Guid UserId { get; set; }
-        public string Name { get; set; }
-        public string eMail { get; set; }
-        public string password { get; set; }
+        public string? Name { get; set; }
+        public string? eMail { get; set; }
+        public string? password { get; set; }
 
     }
 }
