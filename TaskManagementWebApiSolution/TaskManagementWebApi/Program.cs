398bfc5925d734d521c5ffe0e1388f38961564e5
Sumeyra17050111029 <45365584+sumeyrayanardag@users.noreply.github.com>
2022-06-18 14:55:46 +0300

    program.cs updated

--- TaskManagementWebApiSolution/TaskManagementWebApi/Program.cs
+++ TaskManagementWebApiSolution/TaskManagementWebApi/Program.cs
@@ -1,3 +1,14 @@
+
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
+
+string dbProviderName = "Microsoft.Data.SqlClient";
+DbProviderFactories.RegisterFactory(dbProviderName, SqlClientFactory.Instance);
+DbProviderFactories.RegisterFactory("System.Data.SqlClient", SqlClientFactory.Instance);
+
+
+
+
 //using Microsoft.Data.SqlClient;
 
 //string connectionString = "Data Source = localhost; Initial Catalog = TaskManagementdb; Persist Security Info=True; User ID = sa; Password = yourStrong**Password;Encrypt=False";
@@ -12,7 +23,21 @@
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+
+var devCorsPolicy = "devCorsPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(devCorsPolicy, builder => {
+        builder.WithOrigins("http://localhost:8000").AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        //builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        //builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost");
+        //builder.SetIsOriginAllowed(origin => true);
+    });
+});
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,9 +51,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors(devCorsPolicy);
 }
 
-app.UseAuthorization();
+
+//app.UseAuthorization();
 
 app.MapControllers();
 
