599c3ded33e69d13942731c261f4068e4aaeed29
Sumeyra17050111029 <45365584+sumeyrayanardag@users.noreply.github.com>
2022-06-20 11:09:53 +0300

    get login info to localStorage for createBoard

--- Frontend/boardsscript.js
+++ Frontend/boardsscript.js
@@ -21,6 +21,19 @@ $(document).ready(function () {
    
 }); 
 
+window.addEventListener('load', () => {
+ 
+    const username = sessionStorage.getItem('USERNAME');
+    const password = sessionStorage.getItem('PASSWORD');
+    
+    
+    //document.getElementById('result-name').innerHTML = username;
+   // document.getElementById('result-surname').innerHTML = surname;
+   alert("username and password: " + username + password);
+   
+})
+
+
 function getBoards(){
        //on click for <a> element
        $.getJSON( "http://localhost:5288/TaskManagement/get-boards-with-user-id/b418ea3f-4835-4a87-be14-1c99dc3b291e", function( data ) {/*Şimdilik new jsonla deneme yaptım. Çekerken new json yazan yere yukarıdaki urlyi yapıştırıcaz. */
@@ -64,6 +77,52 @@ function getBoards(){
       
      });
 }
+
+
+function createBoard(){
+    //on click for <a> element
+    $.getJSON( "http://localhost:5288/TaskManagement/get-boards-with-user-id/b418ea3f-4835-4a87-be14-1c99dc3b291e", function( data ) {/*Şimdilik new jsonla deneme yaptım. Çekerken new json yazan yere yukarıdaki urlyi yapıştırıcaz. */
+    $.each( data, function(key, value) {
+            console.log("veriler geliyormu");
+            console.log(data[key]);
+        carddiv = document.createElement("div");
+        carddiv.className = "card";
+        
+        cardbody = document.createElement("div");
+        cardbody.className = "card-body";
+        
+        cardtitle = document.createElement("div");
+        cardtitle.className = "card-title";
+        
+        carddesc = document.createElement("div");
+        carddesc.className = "card-desc";
+        
+        seebutton = document.createElement("button");
+        seebutton.className = "button buttonim";
+        seenode = document.createTextNode("See Board");
+        seebutton.appendChild(seenode);
+
+        console.log("veriler geldi mi:");
+        console.log(data[key]);
+        
+        if(data[key].name){
+            cardtitlenode = document.createTextNode(data[key].name);
+            cardtitle.appendChild(cardtitlenode);
+        }
+        element.appendChild(carddiv);
+        
+        carddiv.appendChild(cardbody);
+        
+        cardbody.appendChild(cardtitle);
+        cardbody.appendChild(carddesc);
+        cardbody.appendChild(seebutton);
+
+
+    });
+   
+  });
+}
+
 
 
 /* $.getJSON( "http://localhost:5288/TaskManagement", function( data ) {
